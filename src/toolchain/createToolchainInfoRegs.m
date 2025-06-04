a5357beae446e9134c5a9f8b967c1037493f642c
Tom Anderson <toma@mathworks.com>
2025-06-04 12:02:12 +0100

    Add mingw toolchain

--- src/toolchain/createToolchainInfoRegs.m
+++ src/toolchain/createToolchainInfoRegs.m
@@ -10,23 +10,30 @@ files = dir(here + "/*.mat");
 configs = coder.make.ToolchainInfoRegistry.empty(1,0);
 
 for i = 1:numel(files)
-    pth = files(i).folder;
-    file = files(i).name;
-    ver = string(extractBetween(file, "VS", " x"));
-    bits = string(extractBetween(file, " x", ".mat"));
-
-    configs(i)                       = coder.make.ToolchainInfoRegistry;
-    configs(i).Name                  = "CIGRE DLL - Visual Studio " + ver + " - " + bits + "bit | nmake makefile (64-bit Windows)";
-    configs(i).FileName              = fullfile(pth, file);
-    configs(i).Platform              =  {'win64'};
-
-    switch bits
-        case "32"
-            configs(i).TargetHWDeviceType    = {'Intel->x86-32 (Windows32)','AMD->x86-32 (Windows32)','Generic->Unspecified (assume 32-bit Generic)'};
-        case "64"
-            configs(i).TargetHWDeviceType    = {'Intel->x86-64 (Windows64)','AMD->x86-64 (Windows64)','Generic->Unspecified (assume 64-bit Generic)'};
+    try
+        pth = files(i).folder;
+        file = files(i).name;
+        
+        tc = load(fullfile(pth, file)).tc;
+        name = tc.Name;
+        bits = string(extractBetween(file, " x", ".mat"));
+        
+        configs(i)                       = coder.make.ToolchainInfoRegistry;
+        configs(i).Name                  = name;
+        configs(i).FileName              = fullfile(pth, file);
+        configs(i).Platform              =  {'win64'};
+        
+        switch bits
+            case "32"
+                configs(i).TargetHWDeviceType    = {'Intel->x86-32 (Windows32)','AMD->x86-32 (Windows32)','Generic->Unspecified (assume 32-bit Generic)'};
+            case "64"
+                configs(i).TargetHWDeviceType    = {'Intel->x86-64 (Windows64)','AMD->x86-64 (Windows64)','Generic->Unspecified (assume 64-bit Generic)'};
+        end
+        
+    catch me
+        warning(me.message)
     end
-
+    
 end
 
 end
